2315a7f27b14a4d7b235071f7f054995afaa466a
Gagansagar108 <79242532+Gagansagar108@users.noreply.github.com>
2025-04-20 23:51:46 +0530

    addition of string

--- app/services/string_expression_evaluator.rb
+++ app/services/string_expression_evaluator.rb
@@ -1,6 +1,20 @@
 class StringExpressionEvaluator 
     def self.add(input)
       return 0 if input.strip.empty?
+      
+      delimiter = ","
+      numbers_string = input
+  
+      if input.start_with?("//")
+        delimiter_line, numbers_string = input.split("\n", 2)
+        delimiter = delimiter_line[2..]
+      end
+  
+      sum_ = numbers_string
+                  .gsub("\n", delimiter)
+                  .split(delimiter)
+                  .map(&:to_i)
 
+      return sum_
     end
 end
--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -5,8 +5,49 @@ RSpec.describe StringExpressionEvaluator do
   describe '.add' do
 
     it 'returns 0 for empty string' do
-        binding.pry
       expect(described_class.add("")).to eq(0)
     end
+
+    it 'returns number for a single number input' do
+        expect(described_class.add("5")).to eq(5)
+    end  
+    #
+
+    #Empty string returns 0
+
+    #One number returns the number itself
+    
+    #Two comma-separated numbers
+    
+   # Multiple comma-separated numbers
+    
+    #Handle newlines between numbers
+    
+    #Custom delimiter specified with //
+    
+    #Custom delimiter with newline + commas
+    
+   # Negative number raises exception
+    
+    #Multiple negative numbers in exception message
+    
+    #Input with extra whitespace
+    
+    #Input with trailing newline
+    
+    #Custom delimiter that's a special character
+    
+    #Custom delimiter as a multi-character string
+    
+    #Ignore numbers greater than 1000
+    
+    #Input with no numbers
+    
+    #nput with mixed invalid tokens
+
+
+
+
+
   end
 end
